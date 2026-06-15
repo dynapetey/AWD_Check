280b79c6aa01d161d8c70be64fff862f7f465d56
dynap <dynapetey@gmail.com>
2026-06-15 00:46:26 -0500

    modified:   lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/vehicle_provider.dart';
-import 'vehicle_info_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,23 +10,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<void> _processImage(File imageFile) async {
-    final provider = Provider.of<VehicleProvider>(context, listen: false);
-    await provider.processImage(imageFile);
-    
-    if (!mounted) return;
-
-    if (provider.vehicleData != null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const VehicleInfoScreen()),
-      );
-    } else if (provider.errorMessage != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(provider.errorMessage!)),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
